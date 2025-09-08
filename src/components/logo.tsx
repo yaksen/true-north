c40fc5b65e8c85734eca47791424d6ae945409f0
Sandeepa Athukorala <contact.yaksen@gmail.com>
2025-09-08 08:22:59 +0000

    change it to "Yaksen Hub" (_for element span_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,13 +11,9 @@ export function Logo({ className }: { className?: string }) {
         xmlns="http://www.w3.org/2000/svg"
         className="text-primary"
       >
-        <path d="M4 21V8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M20 21V8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M22 5H2" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M19 9C19 6.79086 15.866 5 12 5C8.13401 5 5 6.79086 5 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M16 21H8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M1.5 21.5V8.08579C1.5 7.49421 1.94772 7 2.5 7H6M1.5 21.5H22.5M1.5 21.5L8.5 14.5M22.5 21.5V8.08579C22.5 7.49421 22.0523 7 21.5 7H18M22.5 21.5L15.5 14.5M12 21.5V3.5M12 3.5L16.5 7.5M12 3.5L7.5 7.5M18 7H6M18 7C18 4.79086 15.3137 3 12 3C8.68629 3 6 4.79086 6 7M18 7V2.5M6 7V2.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
-      <span className="text-xl font-bold">Yaksen CRM</span>
+      <span className="text-xl font-bold">Yaksen Hub</span>
     </div>
   );
 }
