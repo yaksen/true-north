0f8686f5beba2c4e9d02908b05d4f4ae5e32731a
Sandeepa Athukorala <contact.yaksen@gmail.com>
2025-09-26 05:59:12 +0000

    https://developers.google.com/identity/protocols/oauth2

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -66,7 +66,9 @@ export interface Project {
   timezone?: string;
   googleApiKey?: string;
   googleDriveAccessToken?: string;
+  googleDriveRefreshToken?: string;
   googleContactsAccessToken?: string;
+  googleContactsRefreshToken?: string;
 }
 
 export interface Task {
