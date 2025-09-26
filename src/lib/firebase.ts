53e812b04542f598941fa20ef4b9d5b03a14caf4
Sandeepa Athukorala <contact.yaksen@gmail.com>
2025-09-26 05:22:35 +0000

    update the .env too

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBA3nzkgykFZFrlYo8P_WDTg59hsr7e49I",
-  authDomain: "yaksen-crm.firebaseapp.com",
-  projectId: "yaksen-crm",
-  storageBucket: "yaksen-crm.firebasestorage.app",
-  messagingSenderId: "41964003868",
-  appId: "1:41964003868:web:eca64815fea3df34f075d9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
@@ -20,3 +20,4 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { app, auth, db, storage };
+
