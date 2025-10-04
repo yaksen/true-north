71d41a446a4d6031525e241827ac56881123f43f
Sandeepa Athukorala <contact.yaksen@gmail.com>
2025-10-04 03:50:19 +0000

    I think API key is missing for gemini use this "AIzaSyD5Dbtq1Sb1YkYvH8_b

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,3 +1,12 @@
 import { config } from 'dotenv';
 config();
 
+import { genkit, type GenkitErrorCode } from 'genkit';
+
+import { googleAI } from '@genkit-ai/googleai';
+
+genkit({
+  plugins: [googleAI({ apiVersion: 'v1beta' })],
+  logLevel: 'debug',
+  enableTracingAndMetrics: true,
+});
