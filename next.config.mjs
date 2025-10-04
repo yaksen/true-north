d2b8e587d7096f5ab2790598d753ca1a29e32858
Sandeepa Athukorala <contact.yaksen@gmail.com>
2025-10-04 08:05:30 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'drive.google.com',
+        hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
